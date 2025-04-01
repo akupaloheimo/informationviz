cfcd8d58298bd343851428575bbe562e56543d1c
akupaloheimo <aku.paloheimo@hotmail.com>
2025-04-01 13:37:02 +0300

    infoviz

---
+++ src/infoviz/helper.py
@@ -0,0 +1,24 @@
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import geopandas as gpd
+import geoplot
+import geodatasets
+
+import pandas as pd 
+import seaborn as sns
+import colorcet as cc
+
+import contextily as ctx
+
+from infoviz.utils import set_rcParams
+
+os.makedirs("../figures", exist_ok=True)
+
+fontsize = 20
+rcParams = {'figure.labelsize':fontsize, 'axes.labelsize':fontsize, 'xtick.labelsize':fontsize, 
+            'ytick.labelsize':fontsize, 'legend.fontsize':fontsize, 'figure.titlesize':fontsize, 
+            'legend.title_fontsize':fontsize, 
+            'axes.titlesize':fontsize, 'legend.frameon':False}
+set_rcParams(**rcParams)
